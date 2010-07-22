ac7965ba0c72240ffb4d93298a2b64220f5ecdd8
XPilot <XPilot@ba8a923f-d780-44b6-badb-29f948e90341>
2010-07-22 18:14:02 +0000

    Added StringEscape to test package.
    git-svn-id: https://subversion.assembla.com/svn/andromeda@24 ba8a923f-d780-44b6-badb-29f948e90341

---
+++ Andromeda/src/com/sc2mod/andromeda/test/StringEscape.java
@@ -0,0 +1,35 @@
+package com.sc2mod.andromeda.test;
+
+/**
+ * Re-escapes strings.
+ * @author XPilot
+ */
+public class StringEscape {
+	public static void main(String[] args) {
+		String s = "\"\\\t\\\"";
+		test(s);
+	}
+	
+	public static void test(String s) {
+		System.out.println(s);
+		System.out.println(reEscape(s));
+	}
+	
+	public static String reEscape(String s) {
+		StringBuffer buffer = new StringBuffer(s.length());
+		for(int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			switch(c) {
+			case '\n': buffer.append("\\n");
+				break;
+			case '\r': buffer.append("\\r");
+				break;
+			case '\t': buffer.append("\\t");
+				break;
+			case '\"': case '\\': buffer.append('\\');
+			default: buffer.append(c);
+			}
+		}
+		return buffer.toString();
+	}
+}
